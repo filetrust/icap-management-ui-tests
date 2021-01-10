6fc8d15df5f09663fd39d095740e7c6a748d0453
Vitaly Fedrunov <vfedrunov@equian.com>
2021-01-10 23:35:16 +0200

    sendFileICAP() was updated

--- src/utils/steps_file.js
+++ src/utils/steps_file.js
@@ -143,12 +143,20 @@ module.exports = function() {
   sendFileICAP: function (fileName, icapDir, testsDir, inputDir, outputDir) {
     const inputPath = `${process.cwd()}/${inputDir}`
     const outputPath = `${process.cwd()}/${outputDir}`
+    const logsName = 'dockerLogs.log'
     // use NodeJS child process to run a bash command in sync way
-    require('child_process').execSync(`cd ${icapDir} && docker run -v ${inputPath}:/opt -v ${outputPath}:/home glasswallsolutions/c-icap-client:manual-v1 -s 'gw_rebuild' -i icap-client-main.uksouth.cloudapp.azure.com -f '/opt/${fileName}' -o /home/${fileName} -v && cd ${testsDir}`, function(err) {
-        if (err) {
-            console.log('err ' + err);
-        }
-      });
+    // create a file for logs
+    require('child_process').execSync(`cd ${icapDir} && rm -f {logsName} && touch -a ${logsName} && cd ${testsDir}`)
+    // run icap client in docker
+    require('child_process').execSync(`cd ${icapDir} && docker run --name=qa-icap-client --rm -v ${inputPath}:/opt -v ${outputPath}:/home glasswallsolutions/c-icap-client:manual-v1 -s 'gw_rebuild' -i icap-client-main.uksouth.cloudapp.azure.com -f '/opt/${fileName}' -o /home/${fileName} -v &> ${logsName} && cd ${testsDir}`)
+    // read logs from file
+    let output = fs.readFileSync(`${icapDir}/${logsName}`);
+    // get file id from logs
+    let fileId = output
+        .toString()
+        .split('X-Adaptation-File-Id: ')[1]
+        .split("\n")[0];
+    return fileId;
   },
   });
 }
