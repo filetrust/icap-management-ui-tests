4db315ac2d3facd2fc04e69660c737e5bf48c678
NadHodeGW <nhode@glasswallsolutions.com>
2021-02-18 11:54:34 +0000

    workflow, package

--- docker/codecept.entrypoint.sh
+++ docker/codecept.entrypoint.sh
@@ -2,17 +2,6 @@
 
 npm install
 
-nohup google-chrome \
-	--no-sandbox \
-	--disable-background-networking \
-	--disable-default-apps \
-	--disable-extensions \
-	--disable-sync \
-	--disable-translate \
-	--headless \
-	--ignore-certificate-errors \
-	--ignore-ssl-errors \
-	--ignore-certificate-errors-spki-list 
 
 tail -f /dev/null
 sleep 3
